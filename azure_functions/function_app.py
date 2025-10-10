0ad2e68559cf9b31db769a50338f665eb3464512
Luis Alvergue <lalver1@gmail.com>
2025-10-10 11:08:22 -0500

    Include additional details about the alert and format for Slack

--- azure_functions/function_app.py
+++ azure_functions/function_app.py
@@ -12,16 +12,60 @@ SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
 FUNCTION_KEY = os.environ.get("AZURE_FUNCTION_KEY")
 APPINSIGHTS_API_KEY = os.environ.get("APPINSIGHTS_API_KEY")
 
-def fetch_log_details(api_link):
+
+def fetch_search_results(api_link: str) -> dict:
+    """
+    Fetches log details from the Search Results API.
+    """
     headers = {"x-api-key": APPINSIGHTS_API_KEY}
-    response = requests.get(api_link, headers=headers)
-    response.raise_for_status()
-    data = response.json()
-    data_str = json.dumps(data, indent=2) # pretty print 2 spaces indent
-    if len(data_str) > 10000:  # 1 byte per char, roughly 10KB, Azure limits to 64KB
-        data_str = data_str[:10000] + "... [truncated]"
-    logging.info(f"Fetched log details: {data_str}")
-    return data["tables"][0]["rows"][0] if data["tables"] else {}
+    try:
+        logging.info(f"Fetching log details from API: {api_link}")
+        response = requests.get(api_link, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        data_str = json.dumps(data, indent=2) # pretty print 2 spaces indent
+        if len(data_str) > 10000:  # 1 byte per character, roughly 10KB, Azure limits to 64KB
+            data_str = data_str[:10000] + "... [truncated]"
+        logging.info(f"Fetched log details: {data_str}")
+        return data["tables"][0] if data["tables"] else {}
+    except:
+        logging.error(f"Error fetching log details from API: {api_link}")
+        return {"error": "Failed to fetch log details."}
+
+
+def select_search_results(data: dict) -> dict:
+    """
+    Selects specific columns from the full log details data.
+    """
+    json_columns: list[dict] = data["columns"]
+    columns = [col["name"] for col in json_columns]
+    rows: list[list[str | None | int]] = data["rows"]
+
+    selected_columns = [
+    "problemId",
+    "outerMessage",
+    "details",
+    "client_City",
+    "client_StateOrProvince",
+    "cloud_RoleInstance"
+    ]
+    selected_columns_indexes = {col: columns.index(col) for col in selected_columns}
+
+    details = {}
+    for row in rows: # only one row expected but loop anyway
+        entry = {col: row[selected_columns_indexes[col]] for col in selected_columns}
+        details.update(entry)
+
+    return details
+
+def dict_to_bold_string(data: dict) -> str:
+    """
+    Converts a dictionary into a formatted string with bolded keys for Slack messages.
+    """
+    if not data:
+        return "_No additional details found._\n"
+    return "\n".join(f"*{key}:* {value}" for key, value in data.items()) + "\n"
+
 
 @app.route(route="alert_to_slack", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
 def alert_to_slack(req: func.HttpRequest) -> func.HttpResponse:
@@ -44,13 +88,10 @@ def alert_to_slack(req: func.HttpRequest) -> func.HttpResponse:
 
     data = alert_data.get("data", {})
     
-    try:
-        data_str = json.dumps(data, indent=2) # pretty print 2 spaces indent
-        if len(data_str) > 10000:  # 1 byte per char, roughly 10KB, Azure limits to 64KB
-            data_str = data_str[:10000] + "... [truncated]"
-        logging.info(f"Received Azure alert data:\n{data_str}")
-    except Exception as e:
-        logging.warning(f"Failed to serialize alert data for logging: {e}")
+    data_str = json.dumps(data, indent=2) # pretty print 2 spaces indent
+    if len(data_str) > 10000:  # 1 byte per char, roughly 10KB, Azure limits to 64KB
+        data_str = data_str[:10000] + "... [truncated]"
+    logging.info(f"Received Azure alert data:\n{data_str}")
     
     essentials = data.get("essentials", {})
     alert_id = essentials.get("alertId", "N/A")
@@ -62,7 +103,9 @@ def alert_to_slack(req: func.HttpRequest) -> func.HttpResponse:
     alert_context = data.get("alertContext", {})
     condition = alert_context.get("condition", {})
     api_link = condition.get("allOf", [{}])[0].get("linkToSearchResultsAPI", "#")
-    latest_alert_details = fetch_log_details(api_link)
+    search_results = fetch_search_results(api_link)
+    selected_search_results = select_search_results(search_results) if "error" not in search_results else {}
+    details_str = dict_to_bold_string(selected_search_results)
 
     message = (
         f"🚨 *Azure Alert Fired: {alert_rule}*\n\n"
@@ -70,7 +113,7 @@ def alert_to_slack(req: func.HttpRequest) -> func.HttpResponse:
         f"*Date*: {fired_date_time}\n"
         f"*Alert ID*: {alert_id}\n\n"
         f"---------------------------------------------------\n"
-        f"{latest_alert_details}"
+        f"{details_str}"
         f"<{investigation_link}|Click here to investigate in Azure Portal>"
     )
 
@@ -81,10 +124,10 @@ def alert_to_slack(req: func.HttpRequest) -> func.HttpResponse:
         response.raise_for_status()  # Raise an exception for bad status codes
         logging.info(f"Successfully sent message to Slack. Status: {response.status_code}")
         return func.HttpResponse("Alert successfully forwarded to Slack.", status_code=200)
-
     except requests.exceptions.RequestException as e:
         logging.error(f"Error sending message to Slack: {e}")
         return func.HttpResponse(f"Error sending to Slack: {e}", status_code=500)
+
 
 @app.route(route="health", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])
 def health_check(req: func.HttpRequest) -> func.HttpResponse:
