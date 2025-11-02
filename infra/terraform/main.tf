ddfcf6cd079207a41df6d0ca1fa5713bb5ca6d1d
Luis Alvergue <lalver1@gmail.com>
2025-11-02 12:08:02 -0600

    Clean up Terraform definition

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,10 +10,10 @@ terraform {
     }
   }
   backend "azurerm" {
-        resource_group_name  = "web-flask-aca-rg"
-        storage_account_name = "lalver1azurelearning"
+        resource_group_name  = "rg-aca-web"
+        storage_account_name = "salalver1al"
         container_name       = "tfstate"
-        key                  = "web-aca-app.tfstate"
+        key                  = "terraform.tfstate"
       }
 }
 
@@ -25,31 +25,62 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 # 1. Resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "web-flask-aca-rg"
+# Initially created manually to hold the tfstate storage account
+# Import once before the first terraform apply
+resource "azurerm_resource_group" "main" {
+  name     = "rg-aca-web"
   location = "West US"
 }
 
-# 2. Container App Environment
-resource "azurerm_container_app_environment" "env" {
-  name                = "web-aca-env"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# 2. Storage Account
+# Initially created manually to hold the tfstate file
+# Import once before the first terraform apply
+resource "azurerm_storage_account" "main" {
+  name                          = "salalver1al"
+  resource_group_name           = azurerm_resource_group.main.name
+  location                      = azurerm_resource_group.main.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  public_network_access_enabled = true
+
+  blob_properties {
+    last_access_time_enabled = true
+    versioning_enabled       = true
+
+    container_delete_retention_policy {
+      days = 7
+    }
+
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
+  }
 }
 
-# 3. Container App
-resource "azurerm_container_app" "app" {
-  name                         = "web-aca-app"
-  container_app_environment_id = azurerm_container_app_environment.env.id
-  resource_group_name          = azurerm_resource_group.rg.name
+# 3. Container App Environment
+resource "azurerm_container_app_environment" "main" {
+  name                = "web-aca-env"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# 4. Container App - Web
+resource "azurerm_container_app" "web" {
+  name                         = "aca-web"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
   revision_mode                = "Single"
 
   ingress {
     external_enabled = true
-    target_port      = 50505 # match the port your Flask app listens on
+    target_port      = 50505 # match the port the Flask app listens on
     traffic_weight {
       latest_revision = true
-      # revision_suffix = var.container_tag
       percentage=100
       }
     transport        = "auto"
@@ -57,46 +88,44 @@ resource "azurerm_container_app" "app" {
 
   template {
     container {
-      name   = "flask-app"
+      name   = "web"
       image  = "ghcr.io/lalver1/azure-learning:${var.container_tag}"
       cpu    = 0.5
       memory = "1.0Gi"
     
     env {
         name  = "APPLICATIONINSIGHTS_CONNECTION_STRING"
-        value = azurerm_application_insights.appi.connection_string
+        value = azurerm_application_insights.main.connection_string
       }
     }
   }
 }
 
-# 4. Log Analytics Workspace (needed for App Insights)
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "web-flask-law"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# 5. Log Analytics Workspace (needed for App Insights)
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "law-web"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
-# 5. Application Insights
-resource "azurerm_application_insights" "appi" {
-  name                = "web-flask-appi"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# 6. Application Insights
+resource "azurerm_application_insights" "main" {
+  name                = "appi-web"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.law.id
+  workspace_id        = azurerm_log_analytics_workspace.main.id
 }
 
-# 6. Key Vault
-resource "azurerm_key_vault" "kv" {
-  name                        = "webflaskkv4"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
+# 7. Key Vault
+resource "azurerm_key_vault" "main" {
+  name                        = "kv-al"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-
-  soft_delete_retention_days  = 7
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
@@ -119,32 +148,23 @@ resource "azurerm_key_vault" "kv" {
   }
 }
 
-# 7. Key Vault Secret
+# 8. Key Vault Secret
 data "azurerm_key_vault_secret" "slack_webhook_url" {
   name         = "slack-webhook-url"
-  key_vault_id = azurerm_key_vault.kv.id
+  key_vault_id = azurerm_key_vault.main.id
 }
 
-# 8. Storage Account for Function App (for runtime + code package)
-resource "azurerm_storage_account" "funcsa" {
-  name                     = "webflaskfuncsa"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Generate a secure, random key for the function's webhook URL
+# 9. A secure, random key for the Azure Functions' webhook URL
 resource "random_string" "function_key" {
   length  = 32
   special = false
 }
 
-# Deploy the Function App as a container
-resource "azurerm_container_app" "func_app_aca" {
-  name                         = "web-flask-func-aca"
-  container_app_environment_id = azurerm_container_app_environment.env.id
-  resource_group_name          = azurerm_resource_group.rg.name
+# 10. Container App - Azure Functions
+resource "azurerm_container_app" "funcs" {
+  name                         = "aca-funcs"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
   revision_mode                = "Single"
 
   # Securely store secrets that will be injected as environment variables
@@ -158,23 +178,23 @@ resource "azurerm_container_app" "func_app_aca" {
   }
   secret {
     name  = "appinsights-api-key-secret"
-    value = azurerm_application_insights_api_key.appi_api_key.api_key
+    value = azurerm_application_insights_api_key.main.api_key
   }
 
   template {
     container {
-      name   = "function-app-container"
+      name   = "functions"
       image  = "ghcr.io/lalver1/azure-functions:${var.container_tag}"
       cpu    = 0.25
       memory = "0.5Gi"
 
       env {
         name  = "APPLICATIONINSIGHTS_CONNECTION_STRING"
-        value = azurerm_application_insights.appi.connection_string
+        value = azurerm_application_insights.main.connection_string
       }
       env {
         name  = "AzureWebJobsStorage"
-        value = azurerm_storage_account.funcsa.primary_connection_string
+        value = azurerm_storage_account.main.primary_connection_string
       }
       env {
         name        = "SLACK_WEBHOOK_URL"
@@ -210,24 +230,24 @@ resource "azurerm_container_app" "func_app_aca" {
 }
 }
 
-# 14. Action Group that posts to your Function App
-resource "azurerm_monitor_action_group" "func_webhook" {
-  name                = "funcapp-webhook-ag"
-  resource_group_name = azurerm_resource_group.rg.name
+# 11. Action Group that posts to the Functions App
+resource "azurerm_monitor_action_group" "main" {
+  name                = "ag-funcapp-webhook"
+  resource_group_name = azurerm_resource_group.main.name
   short_name          = "funcag"
 
   webhook_receiver {
     name        = "funcapp-webhook"
-    service_uri = "https://${azurerm_container_app.func_app_aca.ingress[0].fqdn}/api/alert_to_slack?CODE=${random_string.function_key.result}"
+    service_uri = "https://${azurerm_container_app.funcs.ingress[0].fqdn}/api/alert_to_slack?CODE=${random_string.function_key.result}"
   }
 }
 
-# 15. Log Search alert rule
-resource "azurerm_monitor_scheduled_query_rules_alert_v2" "app_error" {
-  name                = "web-flask-app-error"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  scopes              = [azurerm_application_insights.appi.id]
+# 12. Log Search alert rule
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "main" {
+  name                = "qr-error"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  scopes              = [azurerm_application_insights.main.id]
 
   description  = "Alerts when any exception is logged in Application Insights."
   display_name = "Web Flask App Error Alert"
@@ -252,7 +272,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "app_error" {
   }
 
   action {
-    action_groups = [azurerm_monitor_action_group.func_webhook.id]
+    action_groups = [azurerm_monitor_action_group.main.id]
     custom_properties = {
       subject = "🚨 Application Error"
     }
@@ -263,19 +283,12 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "app_error" {
   }
 }
 
-# 16. Create an API key for querying Application Insights data
-resource "azurerm_application_insights_api_key" "appi_api_key" {
-  name                    = "funcapp-api-query-key"
-  application_insights_id = azurerm_application_insights.appi.id
+# 13. Create an API key for querying Application Insights data
+resource "azurerm_application_insights_api_key" "main" {
+  name                    = "api-query-key-funcs"
+  application_insights_id = azurerm_application_insights.main.id
 
   read_permissions = [
     "search",
   ]
-}
-
-
-output "current_function_master_key" {
-  value       = random_string.function_key.result
-  sensitive   = true
-  description = "The current master key for the function app. Use this for testing."
 }
